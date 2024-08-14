01eb4b40a3a6e63bb80f5217a479a9374c2881bd
akmiller01 <alex.k.miller@gmail.com>
2024-08-14 10:35:36 -0400

    Project data too large to commit

--- cash_projects.R
+++ cash_projects.R
@@ -1,13 +1,21 @@
-project2016 <- fread("C:/git/CVA-FTS-KWS/all_projects_2016.csv")
-project2017 <- fread("C:/git/CVA-FTS-KWS/all_projects_2017.csv")
-project2018 <- fread("C:/git/CVA-FTS-KWS/all_projects_2018.csv")
-project2019 <- fread("C:/git/CVA-FTS-KWS/all_projects_2019.csv")
-project2020 <- fread("C:/git/CVA-FTS-KWS/all_projects_2020.csv")
-project2021 <- fread("C:/git/CVA-FTS-KWS/all_projects_2021.csv")
-project2022 <- fread("C:/git/CVA-FTS-KWS/all_projects_2022.csv")
-project2023 <- fread("C:/git/CVA-FTS-KWS/all_projects_2023.csv")
+suppressPackageStartupMessages(lapply(c("data.table", "jsonlite","rstudioapi", "httr"), require, character.only=T))
 
-all_projects <- rbind(project2016, project2017, project2018, project2019, project2020, project2021, project2022, project2023)
+setwd(dirname(getActiveDocumentContext()$path))
+
+project2017 <- fread("projects/project_data_2017.csv")
+project2018 <- fread("projects/project_data_2018.csv")
+project2019 <- fread("projects/project_data_2019.csv")
+project2020 <- fread("projects/project_data_2020.csv")
+project2021 <- fread("projects/project_data_2021.csv")
+project2022 <- fread("projects/project_data_2022.csv")
+project2023 <- fread("projects/project_data_2023.csv")
+project2024 <- fread("projects/project_data_2024.csv")
+
+all_projects <- rbindlist(
+  list(
+    project2017, project2018, project2019, project2020, project2021, project2022, project2023, project2024
+    )
+)
 questions <- unique(all_projects$question)
 write.csv(questions, "questions.csv", fileEncoding = "UTF-8", row.names = FALSE, quote = TRUE)
 
@@ -43,13 +51,15 @@ cash.noncase.keywords <- c(
   "CFW",
   "CFA",
   "SSN",
-  "ESSN")
+  "ESSN",
+  "MPC",
+  "MPCT")
 
 cash.noncase.keywords <- paste(cash.noncase.keywords, collapse = "|")
 cash_projects <- all_projects[grepl(cash.noncase.keywords, question)]
 pattern <- "\\d+\\.\\d+|\\d+%|\\d+"
 cash_projects <- cash_projects[grepl(pattern, answer)]
-cash_projects <- cash_projects[!grepl("beneficiaries|justification|is it better", question, ignore.case = TRUE)]
+cash_projects <- cash_projects[!grepl("beneficiaries|justification|is it better|people", question, ignore.case = TRUE)]
 
 
 standardize_percentage <- function(x) {
@@ -77,3 +87,8 @@ standardize_percentage <- function(x) {
     return(num)
 }
 cash_projects <- cash_projects[, standardized_percentage := sapply(answer, standardize_percentage)]
+
+cash_projects = cash_projects[,.(cva_percentage = sum(standardized_percentage)), by=.(project_id)]
+cash_projects$cva_percentage[which(cash_projects$cva_percentage > 100)] = 100
+cash_projects$cva_percentage = cash_projects$cva_percentage / 100
+fwrite(cash_projects, "projects/cash_project_percentages.csv")
--- fetch_projects.R
+++ fetch_projects.R
@@ -2,70 +2,62 @@ suppressPackageStartupMessages(lapply(c("data.table", "jsonlite","rstudioapi", "
 
 setwd(dirname(getActiveDocumentContext()$path))
 
-base_path <- "C:/Users/mike.pearson/Development Initiatives Poverty Research Limited/Humanitarian Team - Documents/GHA Programme/OA1 - Crisis trends, needs, resource flows/GHA Report 2024/Datasets/FTS - Full Dataset/Datasets/"
-year <- 2023
-filename = paste0(base_path, "fts_curated_", year, ".csv")
-fts <- fread(filename)
-
-unique_project_ids <- unique(fts$destinationObjects_Project.id)
-unique_project_ids <- unique_project_ids[complete.cases(unique_project_ids)]
-
-base_url = "https://api.hpc.tools/v2/public/project/"
-
-project_list <- list()
-project_index <- 1
-pb <- txtProgressBar(max = length(unique_project_ids), style = 3)
-for (i in 1: length(unique_project_ids)) {
-  setTxtProgressBar(pb, i)
-  project_id <- unique_project_ids[i]
-  project_url <- paste0(base_url, project_id)
-  project_json <- fromJSON(project_url, simplifyVector = FALSE)
-  
-  
-  
-  project = project_json$data$projectVersion
-  global_clusters_json = project$globalClusters
-  global_clusters = c()
-  for(global_cluster in global_clusters_json){
-    global_clusters = c(global_clusters, global_cluster$name)
-  }
-  global_clusters_string = paste0(global_clusters, collapse=" | ")
-  organisation_json = project$organizations
-  organisation_ids = c()
-  organisation_names = c()
-  for(organisation in organisation_json){
-    organisation_ids = c(organisation_ids, organisation$id)
-    organisation_names = c(organisation_names, organisation$name)
-  }
-  organisation_ids_string = paste0(organisation_ids, collapse=" | ")
-  organisation_names_string = paste0(organisation_names, collapse=" | ")
-  field_definitions = list()
-  for(def in project$plans[[1]]$conditionFields){
-    field_definitions[[as.character(def$id)]] = def
-  }
-  
-  field_values = project$projectVersionPlans[[1]]$projectVersionFields
-  if(length(field_values) == 0){
-    project_df = data.frame(
-      "project_id" = project_id,
-      "project_year" = year,
-      "currently_requested_funds" = project$currentRequestedFunds,
-      "plan_id" = project$plans[[1]]$planVersion$id,
-      "plan_name" = project$plans[[1]]$planVersion$name,
-      "global_clusters" = global_clusters_string,
-      "organisation_ids" = organisation_ids_string,
-      "organisation_names" = organisation_names_string,
-      "question" = "No field questions",
-      "answer" = "No field answers"
-    )
-    project_list[[project_index]] = project_df
-    project_index = project_index + 1
-  }else{
-    for(field in field_values){
-      def = field_definitions[[as.character(field$conditionFieldId)]]
-      if(!is.null(def) & !is.null(field$value)){
+for(year in c(2017:2024)){
+    message(year)
+    base_path <- "https://raw.githubusercontent.com/devinit/gha_automation/main/IHA/datasets/fts_curated_master/"
+    filename = paste0(base_path, "fts_curated_", year, ".csv")
+    fts <- fread(filename)
+    
+    unique_project_ids <- unique(fts$destinationObjects_Project.id)
+    unique_project_ids <- unique_project_ids[complete.cases(unique_project_ids)]
+    
+    base_url = "https://api.hpc.tools/v2/public/project/"
+    
+    project_list <- list()
+    project_index <- 1
+    pb <- txtProgressBar(max = length(unique_project_ids), style = 3)
+    for (i in 1: length(unique_project_ids)) {
+      setTxtProgressBar(pb, i)
+      project_id <- unique_project_ids[i]
+      if(project_id == ""){
+        next
+      }
+      project_url <- paste0(base_url, project_id)
+      project_json <- fromJSON(project_url, simplifyVector = FALSE)
+      
+      
+      
+      project = project_json$data$projectVersion
+      project_objective = ""
+      if(!is.null(project$objective)){
+        project_objective = project$objective
+      }
+      global_clusters_json = project$globalClusters
+      global_clusters = c()
+      for(global_cluster in global_clusters_json){
+        global_clusters = c(global_clusters, global_cluster$name)
+      }
+      global_clusters_string = paste0(global_clusters, collapse=" | ")
+      organisation_json = project$organizations
+      organisation_ids = c()
+      organisation_names = c()
+      for(organisation in organisation_json){
+        organisation_ids = c(organisation_ids, organisation$id)
+        organisation_names = c(organisation_names, organisation$name)
+      }
+      organisation_ids_string = paste0(organisation_ids, collapse=" | ")
+      organisation_names_string = paste0(organisation_names, collapse=" | ")
+      field_definitions = list()
+      for(def in project$plans[[1]]$conditionFields){
+        field_definitions[[as.character(def$id)]] = def
+      }
+      
+      field_values = project$projectVersionPlans[[1]]$projectVersionFields
+      if(length(field_values) == 0){
         project_df = data.frame(
           "project_id" = project_id,
+          "project_name" = project$name,
+          "project_objective" = project_objective,
           "project_year" = year,
           "currently_requested_funds" = project$currentRequestedFunds,
           "plan_id" = project$plans[[1]]$planVersion$id,
@@ -73,15 +65,39 @@ for (i in 1: length(unique_project_ids)) {
           "global_clusters" = global_clusters_string,
           "organisation_ids" = organisation_ids_string,
           "organisation_names" = organisation_names_string,
-          "question" = def$name,
-          "answer" = field$value
+          "question" = "No field questions",
+          "answer" = "No field answers"
         )
         project_list[[project_index]] = project_df
         project_index = project_index + 1
+    }else{
+        for(field in field_values){
+          def = field_definitions[[as.character(field$conditionFieldId)]]
+          if(!is.null(def) & !is.null(field$value)){
+            project_df = data.frame(
+              "project_id" = project_id,
+              "project_name" = project$name,
+              "project_objective" = project_objective,
+              "project_year" = year,
+              "currently_requested_funds" = project$currentRequestedFunds,
+              "plan_id" = project$plans[[1]]$planVersion$id,
+              "plan_name" = project$plans[[1]]$planVersion$name,
+              "global_clusters" = global_clusters_string,
+              "organisation_ids" = organisation_ids_string,
+              "organisation_names" = organisation_names_string,
+              "question" = def$name,
+              "answer" = field$value
+            )
+            project_list[[project_index]] = project_df
+            project_index = project_index + 1
+          }
+        }
       }
-    }}}
+    }
 
-close(pb)
-all_projects <- rbindlist(project_list)
-write.csv(all_projects, "all_projects_2023.csv", fileEncoding = "UTF-8", row.names = FALSE, quote = TRUE)
+  close(pb)
+  all_projects <- rbindlist(project_list)
+  fwrite(all_projects, paste0("projects/project_data_",year,".csv"))
+}
+
 
