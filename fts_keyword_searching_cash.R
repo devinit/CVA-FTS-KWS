563a465f916f74b89ac6eaba10a9722d53406ce5
akmiller01 <alex.k.miller@gmail.com>
2024-08-14 14:21:41 -0400

    Update keyword search

--- fts_keyword_searching_cash.R
+++ fts_keyword_searching_cash.R
@@ -3,7 +3,7 @@ suppressPackageStartupMessages(lapply(c("data.table", "jsonlite","rstudioapi"), 
 setwd(dirname(getActiveDocumentContext()$path))
 
 ##load in fts
-years <- 2016:2021
+years <- 2017:2024
 fts_curated <- list()
 for (i in 1:length(years)){
   year <- years[i]
@@ -12,11 +12,22 @@ for (i in 1:length(years)){
   
 }
 
-fts <- rbindlist(fts_curated)
-fts <- fts[as.character(year) >= 2016]
+fts <- rbindlist(fts_curated, use.names=T)
+fts <- fts[as.character(year) >= 2017]
 
+# Load in project data
+project_metadata = fread("projects/cash_projects.csv")
+project_metadata$project_id = as.character(project_metadata$project_id)
+project_text = fread("projects/project_text.csv")
+project_text$text = paste(project_text$project_name, project_text$project_objective)
+project_text[,c("project_name", "project_objective")] = NULL
+project_data = merge(project_text, project_metadata, all=T)
+names(project_data) = c("sourceObjects_Project.id", "project_text", "project_cva_percentage", "project_cva")
 ## Maybe add in keep function to have only required columns
+fts$sourceObjects_Project.id = as.character(fts$sourceObjects_Project.id)
+fts = merge(fts, project_data, by="sourceObjects_Project.id", all.x=T)
 
+fts$all_text = paste(fts$description, fts$project_text)
 
 ##Keywords from Nik's keyword list, can we tweak so acroynms only picked up if standalone?
 
@@ -35,8 +46,10 @@ cash.noncase.keywords <- c(
   "espèces",
   "monnaie",
   "monétaires",
+  "monétaire",
   "tokens",
   "coupons",
+  "cupones",
   "public works programme",
   "social assistance",
   "social safety net",
@@ -58,8 +71,11 @@ cash.acronyms <- c(
   "CVA"
 )
 
-#creates list of all keywords with case sensitive modifiers where appropriate
-cash.keywords <- paste0("(?i)", paste0(cash.noncase.keywords, collapse = "|"), "|(?-i)", paste0(cash.acronyms, collapse = "|"))
+cash_regex = paste0(
+  "\\b",
+  paste(c(tolower(cash.noncase.keywords), tolower(cash.acronyms)), collapse="\\b|\\b"),
+  "\\b"
+)
 
 ##Relevant clusters from cluster mapping
 cash_clusters <- c(
@@ -87,22 +103,22 @@ cash_clusters <- c(
 fts$relevance <- "None"
 
 ## Define relevance based on sector and/or method
-fts[method == "Cash transfer programming (CTP)", relevance := "Total"]
-fts[destinationObjects_Cluster.name %in% cash_clusters, relevance := "Total"]
+fts[method == "Cash transfer programming (CTP)", relevance := "Full"]
+fts[destinationObjects_Cluster.name %in% cash_clusters, relevance := "Full"]
 
 #TODO select partial sectors with cash cluster and
 fts[grepl(";", destinationObjects_Cluster.name) == T & grepl(paste0(cash_clusters, collapse = "|"), destinationObjects_Cluster.name), relevance := "Partial"]
 ## was by use of grepl | and if cash_clusters == T??
 
 #Count number of keywords appearing in description
-fts[, keywordcount := unlist(lapply(paste0(description), function(x) sum(gregexpr(paste0(cash.keywords, collapse = "|"), x)[[1]] > 0, na.rm = T)))]
-
+fts$keyword_match = grepl(cash_regex, fts$all_text, ignore.case=T)
+mean(fts$keyword_match > 0)
 ##below checks where relevance is none and there are or are not keywords
 ##second line below useful for identifying new keywords maybe missing
-fts[relevance == "None" & keywordcount > 0]
-fts[relevance != "None" & keywordcount == 0]
+View(fts[relevance == "None" & keyword_match][,"all_text"])
+View(fts[relevance != "None" & !keyword_match][,"all_text"])
 
-fts_flagged <- fts[keywordcount > 0 | relevance != "None"]
+fts_flagged <- fts[keyword_match | relevance != "None"]
 
-write.csv(fts_flagged, "fts_output_CVA.csv", fileEncoding = "UTF-8", row.names = F)
+fwrite(fts_flagged, "fts_output_CVA.csv")
 #
